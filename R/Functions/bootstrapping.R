d2798854e99d19c2972e81da0abb3f7359249bea
audhalbritter <me@audhalbritter.com>
2022-11-16 16:36:25 +0100

    remove unused code

--- R/Functions/bootstrapping.R
+++ R/Functions/bootstrapping.R
@@ -6,20 +6,18 @@ make_bootstrapping <- function(community, traits){
 
   #prepare community data
   comm <- community %>%
-    filter(Year == 2015) %>%
-    mutate(Site_trt = paste0(Site, Treatment))
+    filter(Year == 2015)
 
   #prepare trait data
   trait <- traits %>%
-    select(Treatment, Site, PlotID, Taxon, Trait, Value) %>%
-    mutate(Site_trt = paste0(Site, Treatment))
+    select(Treatment, Site, PlotID, Taxon, Trait, Value)
 
 
   #set seed for bootstrapping repeatability
   set.seed(2525)
   trait_imp <- trait_impute(comm = comm,
                             traits = trait,
-                            scale_hierarchy = c("Site", "Site_trt", "PlotID"),
+                            scale_hierarchy = c("Treatment", "PlotID"),
                             global = F,
                             taxon_col = "Taxon",
                             trait_col = "Trait",
@@ -32,13 +30,11 @@ make_bootstrapping <- function(community, traits){
   CWM <- trait_np_bootstrap(trait_imp, nrep = 100, sample_size = 200)
 
   CWM_mean <- trait_summarise_boot_moments(CWM) %>%
-    select(Site:mean)
+    select(Treatment:mean)
 
   #prepare bootstrapped trait data for analyses
-  trait_mean <- CWM_mean %>% ungroup() %>%
-    mutate(Trait = plyr::mapvalues(Trait, from = c("P_percent", "dC13_permil", "dN15_permil"), to = c("P_Ave", "dC13_percent", "dN15_percent"))) %>%
-    mutate(Treatment = substr(Site_trt, 3, 6)) %>%
-    mutate(Site = factor(Site, levels = c("SB", "CH", "DH")))
+  trait_mean <- CWM_mean %>%
+    ungroup()
 
 }
 
