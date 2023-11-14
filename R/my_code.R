52c64526b29f32fd3734a1cf887550bbc85c64df
audhalbritter <me@audhalbritter.com>
2023-11-14 13:34:58 +0100

    edit code

--- R/my_code.R
+++ R/my_code.R
@@ -1,4 +1,4 @@
-### Script to test how warming affects plant height in Bistorta vivipara.
+### Script to test how marine nutrients affect leaf area in Alopecurus magellanicus
 
 # load libraries
 library(tidyverse)
@@ -8,7 +8,7 @@ library(performance)
 # import data
 raw_traits <- read_delim(file = here("data/PFTC4_Svalbard_2018_Gradient_Traits.csv"))
 
-# data cleaning
+# clean data and prepare for analysis
 traits <- raw_traits |>
   # remove NAs
   filter(!is.na(Value)) |>
@@ -16,21 +16,19 @@ traits <- raw_traits |>
   mutate(Gradient = case_match(Gradient,
                                "C" ~ "Control",
                                "B" ~ "Nutrients"),
-         Gradient = factor(Gradient, levels = c("Control", "Nutrients")))
-
-# prepare data for analysis and filter bistorta species and plant height
-bistorta <- traits |>
+         Gradient = factor(Gradient, levels = c("Control", "Nutrients"))) |>
+  # select one species and one trait
   filter(Taxon == "alopecurus magellanicus",
          Trait == "Leaf_Area_cm2")
 
 # run a linear model
-mod_height <- lm(Value ~ Gradient, data = bistorta)
-summary(mod_height)
+mod_area <- lm(Value ~ Gradient, data = traits)
+summary(mod_area)
 # check model assumptions
-check_model(mod_height)
+check_model(mod_area)
 
-# plot treatments vs plant height
-ggplot(bistorta, aes(x = Gradient, y = Value)) +
+# make figure
+ggplot(traits, aes(x = Gradient, y = Value)) +
   geom_boxplot(fill = c("grey80", "darkgreen")) +
-  labs(x = "", y = "Plant height (cm)") +
+  labs(x = "", y = expression(Leaf~area~cm^2)) +
   theme_bw()
